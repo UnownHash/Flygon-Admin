0b1b7aa5775eaea380af03f089c565fe15c5aa89
TurtIeSocks <58572875+TurtIeSocks@users.noreply.github.com>
2023-03-28 10:46:22 -0400

    log url if error

--- server/routes/proxy.go
+++ server/routes/proxy.go
@@ -12,60 +12,60 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func GetSecret(dest string) (header string, secret string) {
+func getSecret(dest string) (string, string) {
 	if dest == "flygon" {
 		return "X-FlyGOn-Secret", config.SafeGetString("flygon.api_secret")
 	}
 	return "X-Golbat-Secret", config.SafeGetString("golbat.api_secret")
 }
 
-func GetApiEndpoint(dest string) (url string) {
+func getApiEndpoint(dest string) string {
 	if dest == "flygon" {
 		return config.SafeGetString("flygon.api_endpoint")
 	}
 	return config.SafeGetString("golbat.api_endpoint")
 }
 
-func BuildRequest(dest string, c *gin.Context) (err error) {
-	fullUrl := util.JoinUrl(GetApiEndpoint(dest), c.Request.RequestURI)
+func buildRequest(dest string, c *gin.Context) (string, error) {
+	fullUrl := util.JoinUrl(getApiEndpoint(dest), c.Request.RequestURI)
 
 	req, err := http.NewRequest(c.Request.Method, fullUrl, c.Request.Body)
 
 	if err != nil {
-		return err
+		return fullUrl, err
 	}
 
-	req.Header.Set(GetSecret(dest))
+	req.Header.Set(getSecret(dest))
 
 	httpClient := &http.Client{}
 	res, err := httpClient.Do(req)
 
 	if err != nil {
-		return err
+		return fullUrl, err
 	}
 
 	defer req.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return err
+		return fullUrl, err
 	}
 
 	var data interface{}
 	err = json.Unmarshal(body, &data)
 	if err != nil {
-		return err
+		return fullUrl, err
 	}
 
 	c.JSON(res.StatusCode, data)
-	return nil
+	return fullUrl, nil
 }
 
 func Flygon(c *gin.Context) {
-	err := BuildRequest("flygon", c)
+	fullUrl, err := buildRequest("flygon", c)
 
 	if err != nil {
-		log.Warnf("[FLYGON] %s Error with request: %s", err)
+		log.Warnf("[FLYGON] %s | error with request: %s", fullUrl, err)
 		c.JSON(500, gin.H{"error": err.Error()})
 	}
 }
