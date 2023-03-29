780ba485f72bddd0c5cd66145b00378910f3b08b
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-03-29 10:06:47 -0400

    respect config host/port for vite dev server

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,62 +1,90 @@
 import { resolve } from 'path'
+import fs from 'fs'
 
 import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker'
+import toml from 'toml'
 
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    splitVendorChunkPlugin(),
-    checker({
-      overlay: {
-        initialIsOpen: false,
-      },
-      typescript: {
-        tsconfigPath: resolve(__dirname, 'tsconfig.json'),
-      },
-      eslint: {
-        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
-      },
-    }),
-  ],
-  publicDir: 'public',
-  resolve: {
-    alias: {
-      '@components': resolve(__dirname, './src/components'),
-      '@features': resolve(__dirname, './src/features'),
-    },
-  },
-  esbuild: {
-    legalComments: 'none',
-  },
-  build: {
-    target: ['safari11.1', 'chrome64', 'firefox66', 'edge88'],
-    outDir: resolve(__dirname, '../dist'),
-    sourcemap: mode === 'development',
-    minify: mode === 'development' ? false : 'esbuild',
-    input: { main: resolve(__dirname, 'index.html') },
-    assetsDir: '',
-    emptyOutDir: true,
-  },
-  server: {
-    host: '0.0.0.0',
-    open: true,
-    port: 9004,
-    fs: {
-      strict: false,
-    },
-    proxy: {
-      '/api': {
-        target: `http://0.0.0.0:9003`,
-        changeOrigin: true,
-        secure: false,
-      },
-      '/auth': {
-        target: `http://0.0.0.0:9003`,
-        changeOrigin: true,
-        secure: false,
+interface Config {
+  general: {
+    host: string
+    port: number
+    username: string
+    password: string
+    flygon: {
+      api_endpoint: string
+      api_secret: string
+    }
+    golbat: {
+      api_endpoint: string
+      api_secret: string
+    }
+  }
+}
+export default defineConfig(({ mode }) => {
+  const config: Config = fs.existsSync(resolve(__dirname, '../config.toml'))
+    ? toml.parse(fs.readFileSync(resolve(__dirname, '../config.toml'), 'utf-8'))
+    : toml.parse(
+        fs.readFileSync(resolve(__dirname, '../config.toml.example'), 'utf-8'),
+      )
+  const host = config?.general?.host || '0.0.0.0'
+  const port = config?.general?.port || 9003
+
+  return {
+    plugins: [
+      react(),
+      splitVendorChunkPlugin(),
+      checker({
+        overlay: {
+          initialIsOpen: false,
+        },
+        typescript: {
+          tsconfigPath: resolve(__dirname, 'tsconfig.json'),
+        },
+        eslint: {
+          lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+        },
+      }),
+    ],
+    publicDir: 'public',
+    resolve: {
+      alias: {
+        '@components': resolve(__dirname, './src/components'),
+        '@features': resolve(__dirname, './src/features'),
       },
     },
-  },
-}))
+    esbuild: {
+      legalComments: 'none',
+    },
+    build: {
+      target: ['safari11.1', 'chrome64', 'firefox66', 'edge88'],
+      outDir: resolve(__dirname, '../dist'),
+      sourcemap: mode === 'development',
+      minify: mode === 'development' ? false : 'esbuild',
+      input: { main: resolve(__dirname, 'index.html') },
+      assetsDir: '',
+      emptyOutDir: true,
+    },
+    server: {
+      host,
+      open: true,
+      port: port + 1,
+      fs: {
+        strict: false,
+      },
+      proxy: {
+        '/api': {
+          target: `http://${host}:${port}`,
+          changeOrigin: true,
+          secure: false,
+        },
+        '/auth': {
+          target: `http://${host}:${port}`,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  }
+})
