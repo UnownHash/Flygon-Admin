75b58aa8b8da45f84d860260be07d88c2ec28432
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-03-29 15:52:36 -0400

    comment some things out for later

--- client/src/features/area/AreaCreate.tsx
+++ client/src/features/area/AreaCreate.tsx
@@ -4,13 +4,13 @@ import { AreaForm } from './AreaForm'
 import { Area, AreaDTO } from './type'
 
 const transformPayload = (area: Area): AreaDTO => {
-  let hours = []
+  // let hours = []
 
-  try {
-    hours = JSON.parse(area.quest_mode.hours as unknown as string)
-  } catch (error) {
-    console.error(error)
-  }
+  // try {
+  //   hours = JSON.parse(area.quest_mode.hours as unknown as string)
+  // } catch (error) {
+  //   console.error(error)
+  // }
 
   return {
     ...area,
@@ -18,18 +18,18 @@ const transformPayload = (area: Area): AreaDTO => {
       Array.isArray(area.geofence) && area.geofence.length > 0
         ? area.geofence
         : undefined,
-    fort_mode:
-      area.fort_mode.route.length > 0 || area.fort_mode.workers !== 0
-        ? {
-            ...area.fort_mode,
-            route:
-              area.fort_mode.route.length > 0
-                ? area.fort_mode.route
-                : undefined,
-            workers:
-              area.fort_mode.workers !== 0 ? area.fort_mode.workers : undefined,
-          }
-        : undefined,
+    // fort_mode:
+    //   area.fort_mode.route.length > 0 || area.fort_mode.workers !== 0
+    //     ? {
+    //         ...area.fort_mode,
+    //         route:
+    //           area.fort_mode.route.length > 0
+    //             ? area.fort_mode.route
+    //             : undefined,
+    //         workers:
+    //           area.fort_mode.workers !== 0 ? area.fort_mode.workers : undefined,
+    //       }
+    //     : undefined,
     pokemon_mode:
       area.pokemon_mode.route.length > 0 || area.pokemon_mode.workers !== 0
         ? {
@@ -44,26 +44,26 @@ const transformPayload = (area: Area): AreaDTO => {
                 : undefined,
           }
         : undefined,
-    quest_mode:
-      area.quest_mode.max_login_queue ||
-      area.quest_mode.workers !== 0 ||
-      (Array.isArray(hours) && hours.length > 0) ||
-      area.quest_mode.route.length > 0
-        ? {
-            ...area.quest_mode,
-            route:
-              area.quest_mode.route.length > 0
-                ? area.quest_mode.route
-                : undefined,
-            max_login_queue: area.quest_mode.max_login_queue
-              ? area.quest_mode.max_login_queue
-              : undefined,
-            workers: area.quest_mode.workers
-              ? area.quest_mode.workers
-              : undefined,
-            hours: Array.isArray(hours) && hours.length > 0 ? hours : undefined,
-          }
-        : undefined,
+    // quest_mode:
+    //   area.quest_mode.max_login_queue ||
+    //   area.quest_mode.workers !== 0 ||
+    //   (Array.isArray(hours) && hours.length > 0) ||
+    //   area.quest_mode.route.length > 0
+    //     ? {
+    //         ...area.quest_mode,
+    //         route:
+    //           area.quest_mode.route.length > 0
+    //             ? area.quest_mode.route
+    //             : undefined,
+    //         max_login_queue: area.quest_mode.max_login_queue
+    //           ? area.quest_mode.max_login_queue
+    //           : undefined,
+    //         workers: area.quest_mode.workers
+    //           ? area.quest_mode.workers
+    //           : undefined,
+    //         hours: Array.isArray(hours) && hours.length > 0 ? hours : undefined,
+    //       }
+    //     : undefined,
   }
 }
 
--- client/src/features/area/AreaEdit.tsx
+++ client/src/features/area/AreaEdit.tsx
@@ -4,24 +4,24 @@ import { AreaForm } from './AreaForm'
 import { Area } from './type'
 
 const transformPayload = (area: Area): Area => {
-  let hours = []
+  // let hours = []
 
-  try {
-    if (typeof area.quest_mode.hours === 'string') {
-      hours = JSON.parse(area.quest_mode.hours as unknown as string)
-    } else if (Array.isArray(area.quest_mode.hours)) {
-      hours = area.quest_mode.hours
-    }
-  } catch (error) {
-    console.error(error)
-  }
+  // try {
+  //   if (typeof area.quest_mode.hours === 'string') {
+  //     hours = JSON.parse(area.quest_mode.hours as unknown as string)
+  //   } else if (Array.isArray(area.quest_mode.hours)) {
+  //     hours = area.quest_mode.hours
+  //   }
+  // } catch (error) {
+  //   console.error(error)
+  // }
 
   return {
     ...area,
-    quest_mode: {
-      ...area.quest_mode,
-      hours: Array.isArray(hours) && hours.length > 0 ? hours : undefined,
-    },
+    // quest_mode: {
+    //   ...area.quest_mode,
+    //   hours: Array.isArray(hours) && hours.length > 0 ? hours : undefined,
+    // },
   }
 }
 
